0ae7ca4004997d929d1e428d1d4a0ecd3089ad63
judice <eciduj@gmail.com>
2016-12-23 19:50:21 +0800

    12/23

--- app/main/view.py
+++ app/main/view.py
@@ -1,10 +1,63 @@
 from . import main
 from ..models import Todo, TodoForm
-from flask import render_template
+from flask import render_template, request
+
 
 @main.route('/')
 def index():
     form = TodoForm()
     todos = Todo.objects.order_by('-time')
-    return render_template('index.html', form=form, todos=todos)
+    return render_template('index.html', todos=todos,form=form)
+
+
+@main.route('/add', methods=['POST'])
+def add():
+    form = TodoForm(request.form)
+    if form.validate():
+       content = form.content.data
+       todo = Todo(content=content)
+       todo.save()
+    todos = Todo.objects.order_by('-time')
+    return render_template('index.html', todos=todos, form=form)
+
+
+@main.route('/done/<string:t_id>')
+def done(t_id):
+    form = TodoForm()
+    todo = Todo.objects.get_or_404(id=t_id)
+    todo.status = 1
+    todo.save()
+    todos = Todo.objects.order_by('-time')
+    return render_template('index.html',todos=todos,form=form)
+
+
+@main.route('/undone/<string:t_id>')
+def undone(t_id):
+    form = TodoForm()
+    todo = Todo.objects.get_or_404(id=t_id)
+    todo.status = 0
+    todo.save()
+    todos = Todo.objects.order_by('-time')
+    return render_template('index.html',todos=todos, form=form)
+
+
+@main.route('/delete/<string:t_id>')
+def delete(t_id):
+    form = TodoForm()
+    todo = Todo.objects.get_or_404(id=t_id)
+    todo.delete()
+    todo.save()
+    todos = Todo.objects.order_by('-times')
+    return render_template('index.html',todos=todos, form=form)
+
+
+@main.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'),404
+
+
+
+
+
+
 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from app import db
 from flask_mongoengine.wtf import model_form
 import datetime
@@ -8,4 +9,4 @@ class Todo(db.Document):
     time = db.DateTimeField(default=datetime.datetime.now())
     status = db.IntField(default=0)
 
-TodoForm = model_form(Todo)
+TodoForm = model_form(Todo) # 进行表单验证
